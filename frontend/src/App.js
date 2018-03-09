d20ee96016794cbfdf87de5558b6b2477fb10193
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-03-09 14:39:06 +0100

    add URL validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,32 @@ import './App.css';
 import { Download } from 'react-feather';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoURL: '',
+      isValidURL: true
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const isValidYoutube = (/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/).test(event.target.value);
+    if (isValidYoutube) {
+      this.setState({ videoURL: event.target.value, isValidURL: true });
+    }
+    else {
+      (event.target.value === '') ? this.setState({ isValidURL: true }) : this.setState({ isValidURL: false })
+    }
+  }
+
+  handleSubmit(event) {
+    alert('ytURL: ' + this.state.videoURL);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="wrapper">
@@ -12,7 +38,7 @@ class App extends Component {
           <p>Start by pasting link of youtube video or playlist you would like to download</p>
         </div>
         <div className="videoInput">
-          <input type="text"/>
+          <input className={this.state.isValidURL ? '' : 'danger'} onChange={this.handleChange}/>
           <button className="btn">
             <Download className="feather" />
             download
